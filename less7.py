91a6ad6d2c3fa1239ecdcb2dbd4f87bce1ac794f
zhukenin <zhukenin@icloud.com>
2019-02-21 22:31:18 +0800

    less7

---
+++ less7.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+print('please input your age')
+age = int(input())
+if age >= 18:
+    print('Pass')
+else:
+    print('Sorry,You don\t comein')
+
+age = 20
+if age >= 6:
+    print('teenager')
+elif age >= 18:
+    print('adult')
+else:
+    print('kid')
