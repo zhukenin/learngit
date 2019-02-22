29c08a1706f518720e4b1e688073e45cfb8c6c02
zhukenin <zhukenin@icloud.com>
2019-02-22 15:29:09 +0800

    less7.5

---
+++ less7.5.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+print('请输入小明的身高h ,体重w ')
+h = float(input('h: '))
+w = float(input('w: '))
+bmi = int(w/(h*h))
+print('bmi=  %.2f' % bmi)
+if bmi < 18.5:
+    print('过轻')
+elif bmi >=18.5<=25:
+    print('正常')
+elif bmi >=25<=28:
+    print('过重')
+elif bmi >=28<=32:
+    print('肥胖')
+else:
+    print('严重肥胖')
